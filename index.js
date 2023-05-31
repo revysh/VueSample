7f009b113881cfcd7226a67dae2c76b0d6822341
Ravishankar Rajappa <revysh@gmail.com>
2023-05-31 18:51:08 +0400

    Aded v-bind

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ window.onload = function(){
     new Vue({
         el: '#raviapp',
         data: {
-            name:'RaviLoad'
+            name:'RaviLoad',
+            elements: []
         },
         methods: {
             changeName: function(){
                 this.name = 'Ravi Changed' 	
+            },
+            addElement: function(){
+                this.elements.push(this.elements.length+1);
             }
         }
     });
