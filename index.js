7765b1a825869c265fa7eb5ad4c3af777732c13a
Ravishankar Rajappa <revysh@gmail.com>
2023-05-31 12:24:53 +0400

    Added Events

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 window.onload = function(){
     new Vue({
-        el: '#app',
+        el: '#raviapp',
         data: {
-            todos: [],
-            userInput:''
+            name:'RaviLoad'
         },
         methods: {
-            addTodo: function(){
-                if(this.userInput != ''){
-                    var newTodo = {"id":_.uniqueId(), "value": this.userInput};
-                    this.todos.push(newTodo);
-                    console.log(this.todos);
-                    }    	
-            },
-            removeTodo: function(todo){
-                this.todos = _.remove(this.todos,function(eachTodo) {
-                    return eachTodo.id != todo.id;
-                });                
+            changeName: function(){
+                this.name = 'Ravi Changed' 	
             }
         }
     });
