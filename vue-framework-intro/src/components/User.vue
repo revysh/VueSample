f6f93f5f32af3090918967a3bf9eb6d62bcbc253
Ravishankar Rajappa <revysh@gmail.com>
2023-06-02 11:49:40 +0400

    Adding vue projects using cli

---
+++ vue-framework-intro/src/components/User.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="user">
+        <h3>The User</h3>
+        <p> My username is {{ username }}</p>
+        <app-info></app-info>
+    </div>
+</template>
+
+<script>
+    import Info from './Info.vue';
+    export default {
+        data(){
+            return {
+                username: 'Ravi'
+            };
+        },
+        components:{
+            'app-info':Info
+        }
+    }
+</script>
+
+<style>
+    p {
+        font-style: italic;
+    }
+</style>
